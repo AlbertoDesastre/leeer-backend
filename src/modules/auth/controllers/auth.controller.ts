7dfdcf1a3bc8c96d168b34f0f59000cdffafe035
Alberto_Desastre <armassantanaalberto@gmail.com>
2025-05-20 22:57:25 +0100

    added decorator to check for roles and forbid accesses

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,23 +1,14 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  UseGuards,
-  SetMetadata,
-} from '@nestjs/common';
-import { AuthService } from '@/modules/auth/services/auth.service';
+import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
+import { AuthService } from '@/modules/auth/services/auth.service';
 import { RegisterAuthDto } from '@/modules/auth/dto/register-auth.dto';
 import { LoginAuthDto } from '@/modules/auth/dto/login-auth.dto';
-import { Auth } from '../entities/auth.entity';
-import { AuthGuard } from '@nestjs/passport';
-import { GetUser } from '../decorators/get-user.decorator';
 import { User } from '@/modules/users/entities/user.entity';
 import { RolesGuard } from '../guards/auth.guard';
+import { GetUser } from '../decorators/get-user.decorator';
+import { ProtectedByRoles } from '../decorators/decorators.decorator';
+import { VALID_ROLES } from '../interfaces/valid-roles';
 
 @Controller('auth')
 export class AuthController {
@@ -41,8 +32,8 @@ export class AuthController {
   }
 
   @Get('segunda-prueba')
-  @SetMetadata('roles', ['guest', 'original-author', 'collaborator'])
   // Un Guard es una función que permite/previene el acceso a una ruta. Se ejecuta mucho antes que la ruta y el controlador. Fuente: https://docs.nestjs.com/guards
+  @ProtectedByRoles(VALID_ROLES.ORIGINAL_AUTHOR) // Este Guard vacío signfica que dejo entrar a cualquiera (conceptualmente, un guest), y cada rol nuevo que le meta y no cumpla el tipo que me hace la request le salta un Forbiden access
   @UseGuards(AuthGuard(), RolesGuard)
   segundaPrueba(@GetUser() user: User, @GetUser('email') userEmail: string) {
     return { token: user.token, user, userEmail };
