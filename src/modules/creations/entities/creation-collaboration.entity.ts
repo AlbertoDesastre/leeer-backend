a8d26a1470ffa6e7a79b487c576e8a88091dc8a1
Alberto_Desastre <armassantanaalberto@gmail.com>
2025-05-21 23:34:01 +0100

    added creation_collaborationes entity to have author collaborators

--- src/modules/creations/entities/creation-collaboration.entity.ts
+++ src/modules/creations/entities/creation-collaboration.entity.ts
@@ -5,27 +5,24 @@ import {
   Entity,
   JoinColumn,
   ManyToOne,
-  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
-import { CreationCollaboration } from './creation-collaboration.entity';
+import { Creation } from './creation.entity';
 
 /* @Entity marca la tabla real de la BD "creations" para hacer las transacciones. El resto de decoradores son autodefinitorios. */
-@Entity('creations')
-export class Creation {
+@Entity('creation_collaborations')
+export class CreationCollaboration {
   @PrimaryGeneratedColumn('uuid')
-  creation_id: string;
-  @Column({ type: 'varchar', length: 60 })
-  title: string;
+  creation_collaboration_id: string;
+  @Column({ default: false }) // COMPROBAR SI ESTO HACE LO QUE PIENSO!!!
+  approved_by_original_author: boolean;
   @Column()
-  is_draft: boolean;
-  @Column('tinytext')
-  synopsis: string;
-  @Column('text')
-  description: string;
-  @Column('text')
-  thumbnail: string;
+  is_fanfiction: boolean;
+  @Column()
+  is_spin_off: boolean;
+  @Column()
+  is_canon: boolean;
   @Column('timestamp')
   @CreateDateColumn() // decorador genial que me guarda la fecha en la que se creó la entidad en la BBDD
   creation_date: string;
@@ -33,17 +30,17 @@ export class Creation {
   @UpdateDateColumn() // decorador genial que me guarda la fecha en la que se modificó cada vez que se usa el método ".save" del repositorio <3
   modification_date: string;
 
-  // relación con los usuarios + breve explicación de sintaxis
   @ManyToOne(
     () => User, // primero le asigno la Entidad objetivo
-    (user) => user.creations, // luego, dentro de esa entidad, por qué propiedad está conectada
+    (user) => user.creation_collaborations, // luego, dentro de esa entidad, por qué propiedad está conectada
     { eager: true }, // esto carga automáticamente la entidad usuario con la que está relacionado, sin necesidad de hacer un leftJoin
   )
   // He tenido que poner este JoinColumn porque en mi base de dato la columna se llama "user_id" y no "userid", que es lo que TypeORM estaba infiriendo y buscando. Como no lo encontraba en la tabla usuarios, petaba.
   @JoinColumn({ name: 'user_id' })
   user: User;
 
-  @OneToMany(() => CreationCollaboration, (creationCollaboration) => creationCollaboration.creation)
-  @JoinColumn({ name: 'creation_collaboration_id' })
-  creation_collaborations?: CreationCollaboration[];
+  /* De nuevo, le digo en qué columna se tiene que fijar para encontrar esta misma entidad */
+  @ManyToOne(() => Creation, (creation) => creation.creation_collaborations, { eager: true })
+  @JoinColumn({ name: 'creation_id' })
+  creation: Creation;
 }
