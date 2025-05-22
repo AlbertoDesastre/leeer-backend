6b137002edbab78d5d2bb8fb7ed0116a98e66f73
Alberto_Desastre <armassantanaalberto@gmail.com>
2025-05-22 23:37:14 +0100

    added decorator and author guard to check author permisions

--- src/modules/creations/controllers/creations.controller.ts
+++ src/modules/creations/controllers/creations.controller.ts
@@ -19,38 +19,19 @@ import { Authenticate } from '@/modules/auth/decorators/authenticate.decorator';
 import { GetUser } from '@/modules/auth/decorators/get-user.decorator';
 import { User } from '@/modules/users/entities/user.entity';
 import { CreateCollaborationPetitionDto } from '../dto/create-creation-collaboration-petition.dto';
+import { AuthenticateByAuthorOwnership } from '@/modules/auth/decorators/authenticate-by-author-ownership.decorator';
 
 @Controller('creations')
 export class CreationsController {
   constructor(private readonly creationsService: CreationsService) {}
 
-  // CRUD básico para Creations
-  @Post()
-  create(@Body() createCreationDto: CreateCreationDto) {
-    return this.creationsService.create(createCreationDto);
-  }
-
-  @Get()
-  findAll(@Query() paginationDto: PaginationDto) {
-    return this.creationsService.findAll(paginationDto);
-  }
-
-  @Get(':term')
-  findOne(@Param('term') term: string) {
-    return this.creationsService.findOne(term);
-  }
-
-  @Patch(':id')
-  update(@Param('id', ParseUUIDPipe) id: string, @Body() updateCreationDto: UpdateCreationDto) {
-    return this.creationsService.update(id, updateCreationDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id', ParseUUIDPipe) id: string) {
-    return this.creationsService.remove(id);
-  }
-
   // Solicitudes de Colaboración
+  @Get('collaborations')
+  @AuthenticateByAuthorOwnership()
+  getCollaborationPetition() {
+    return 'Estás en el buen recurso';
+  }
+
   @Post('collaborations')
   @Authenticate()
   sendCollaborationPetition(
@@ -63,6 +44,32 @@ export class CreationsController {
       creation_id,
       createCollaborationPetitionDto,
     );
+  }
+
+  // Creations
+  @Post()
+  create(@Body() createCreationDto: CreateCreationDto) {
+    return this.creationsService.create(createCreationDto);
+  }
+
+  @Get()
+  findAll(@Query() paginationDto: PaginationDto) {
+    return this.creationsService.findAll(paginationDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id', ParseUUIDPipe) id: string, @Body() updateCreationDto: UpdateCreationDto) {
+    return this.creationsService.update(id, updateCreationDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.creationsService.remove(id);
+  }
+
+  @Get(':term') // ¡Esta ruta debe ir al final siempre, porque hace mathing dinámico con cualquier cosa! Las rutas específicas, en Nest, van siempre lo más arriba posible.
+  findOne(@Param('term') term: string) {
+    return this.creationsService.findOne(term);
   }
   // TODO: Buscar creaciones por Tags. Se necesita haber creado el módulo de Tags previamente
 }
