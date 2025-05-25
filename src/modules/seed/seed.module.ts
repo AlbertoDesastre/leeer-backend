e7fadb26851edc5d22a44b868b2809f44a8a5615
Alberto_Desastre <armassantanaalberto@gmail.com>
2025-05-25 16:38:00 +0100

    modified seed to create users via AuthModule

--- src/modules/seed/seed.module.ts
+++ src/modules/seed/seed.module.ts
@@ -10,15 +10,22 @@ import { Creation } from '../creations/entities/creation.entity';
 import { CreationsModule } from '../creations/creations.module';
 import { CreationsService } from '../creations/services/creations.service';
 import { CreationCollaboration } from '../creations/entities/creation-collaboration.entity';
+import { UsersService } from '../users/services/users.service';
+import { AuthService } from '../auth/services/auth.service';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { AuthModule } from '../auth/auth.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
     ConfigModule,
+    AuthModule,
+    PassportModule,
     TypeOrmModule.forFeature([User, Creation, CreationCollaboration]),
     CreationsModule,
   ],
   controllers: [SeedController],
-  providers: [SeedService, CreationsService],
-  exports: [SeedModule],
+  providers: [SeedService, CreationsService, AuthService],
+  exports: [],
 })
 export class SeedModule {}
--- src/modules/seed/services/seed.service.ts
+++ src/modules/seed/services/seed.service.ts
@@ -6,28 +6,31 @@ import { usersSeed } from '../data';
 import { CreateCreationDto } from '@/modules/creations/dto/create-creation.dto';
 import { Creation } from '@/modules/creations/entities/creation.entity';
 import { CreationsService } from '@/modules/creations/services/creations.service';
+import { UsersService } from '@/modules/users/services/users.service';
+import { AuthService } from '@/modules/auth/services/auth.service';
 
 @Injectable()
 export class SeedService {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
     @InjectRepository(Creation) private readonly creationRepository: Repository<Creation>,
+    private readonly authService: AuthService,
     private readonly creationService: CreationsService,
   ) {}
 
+  // Utilizo los servicios en vez de los repositorios para insertar porque hay ciertas lógicas como encriptar las contraseñas qeu solo están disponibles ahí.
   async createData() {
     // Como paso previo elimino todo lo anterior
     await this.deleteAllData();
 
-    // creo todos los usuarios
-    try {
-      await this.userRepository.save(usersSeed);
-    } catch (error) {
-      console.log(error);
-    }
-
-    // creo las creaciones genéricas para hacer mis pruebitas
     for (const user of usersSeed) {
+      // creo todos los usuarios
+      try {
+        await this.authService.register(user);
+      } catch (error) {
+        console.log(error);
+      }
+      // creo las creaciones genéricas para hacer mis pruebitas
       const creationDto: CreateCreationDto = {
         user_id: user.user_id,
         title: `Obra de ${user.nickname}`,
