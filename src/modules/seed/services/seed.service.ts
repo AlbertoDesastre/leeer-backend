fb569abcb780b21f128acdd8cee15f6877880faf
Alberto_Desastre <armassantanaalberto@gmail.com>
2025-05-25 16:52:28 +0100

    added method to add creation collaborations automatically

--- src/modules/seed/services/seed.service.ts
+++ src/modules/seed/services/seed.service.ts
@@ -8,6 +8,7 @@ import { Creation } from '@/modules/creations/entities/creation.entity';
 import { CreationsService } from '@/modules/creations/services/creations.service';
 import { UsersService } from '@/modules/users/services/users.service';
 import { AuthService } from '@/modules/auth/services/auth.service';
+import { CreateCollaborationPetitionDto } from '@/modules/creations/dto/create-creation-collaboration-petition.dto';
 
 @Injectable()
 export class SeedService {
@@ -45,12 +46,48 @@ export class SeedService {
         console.log(error);
       }
     }
+
+    await this.findAndCreateCollabPetitions();
+
     return 'Datos insertados en BD.';
+  }
+
+  async findAndCreateCollabPetitions() {
+    const creations = await this.creationService.findAll({ limit: 99, offset: 0 });
+    const allUsers = await this.userRepository.find();
+
+    for (const creation of creations) {
+      const authorId = creation.user.user_id;
+
+      // Selecciono un usuario que no sea autor original
+      const collaborator = allUsers[Math.floor(Math.random() * allUsers.length)]; // Selecciono un usuario al azar para que el colaborador no sea siempre el mismo
+
+      // no ser colaborador es equivalente a ser el autor original, así que se pasa al siguiente
+      if (collaborator.user_id === authorId) continue;
+
+      const collaborationPetition: CreateCollaborationPetitionDto = {
+        approved_by_original_author: false,
+        is_fanfiction: false,
+        is_spin_off: true,
+        is_canon: true,
+      };
+
+      try {
+        await this.creationService.sendCollaborationPetition(
+          collaborator,
+          creation.creation_id,
+          collaborationPetition,
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    }
   }
 
   async deleteAllData() {
     await this.userRepository.deleteAll();
     await this.creationRepository.deleteAll();
+    // no hace falta deletear las colaboraciones porque "Creation" tiene borrado en cascada
 
     console.log('Se eliminó toda la data antes de ejecutar el seed.');
   }
