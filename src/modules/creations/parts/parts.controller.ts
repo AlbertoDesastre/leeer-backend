20d137e0c5c7895914bc72912b584fc2a7cee2df
Alberto_Desastre <armassantanaalberto@gmail.com>
2025-08-09 19:20:33 +0100

    refactor api routes to get a single part from id

--- src/modules/creations/parts/parts.controller.ts
+++ src/modules/creations/parts/parts.controller.ts
@@ -23,12 +23,10 @@ import { GetCreation } from '../decorators/get-creation.decorator';
 export class PartsController {
   constructor(private readonly partsService: PartsService) {}
 
-  // TODO: Cambiar las llamadas GET-ONLY para que acepte un ID al final de la request. Ahora me parece una llamada fea, o tmb puedo cambiarle el nombre en vez de get-only ponerle otra cosa como "unique"
-
   @Post()
   @ApiOperation({ summary: 'Crear una nueva parte en una creación' })
   @ApiBody({ type: CreatePartDto })
-  @ApiResponse({ status: 201, type: GetPartWithFullDetailDto, isArray: true })
+  @ApiResponse({ status: 201, type: GetPartWithFullDetailDto })
   @ApiResponse({ status: 400, description: 'Bad request' })
   // por supuesto que el creador de la obra puede añadir partes, pero también los colaboradores aprobados por él/ella
   @AuthenticateByAuthorOwnership(VALID_ROLES.ORIGINAL_AUTHOR, VALID_ROLES.COLLABORATOR)
@@ -69,29 +67,6 @@ export class PartsController {
     return this.partsService.findAll({ creation_id, paginationDto, showDrafts: true });
   }
 
-  @Get('get-only')
-  @ApiOperation({ summary: 'Obtener una parte pública por ID' })
-  @ApiParam({ name: 'creation_id', type: String })
-  @ApiQuery({ name: 'id', type: String })
-  @ApiResponse({ status: 200, type: GetPartResponseDto })
-  @ApiResponse({ status: 404, description: 'Not Found' })
-  findOne(@Param('creation_id') creation_id: string, @Query('id', ParseUUIDPipe) id: string) {
-    return this.partsService.findOne({ creation_id, id, showDrafts: false });
-  }
-
-  @Get('author/get-only')
-  @ApiOperation({
-    summary: 'Obtener una parte (incluyendo borradores) del autor o colaborador por ID',
-  })
-  @ApiParam({ name: 'creation_id', type: String })
-  @ApiQuery({ name: 'id', type: String })
-  @ApiResponse({ status: 200, type: GetPartResponseDto })
-  @ApiResponse({ status: 404, description: 'Not Found' })
-  @AuthenticateByAuthorOwnership(VALID_ROLES.ORIGINAL_AUTHOR, VALID_ROLES.COLLABORATOR)
-  findMyPart(@Param('creation_id') creation_id: string, @Query('id', ParseUUIDPipe) id: string) {
-    return this.partsService.findOne({ creation_id, id, showDrafts: true });
-  }
-
   @Delete(':id')
   @ApiOperation({ summary: 'Eliminar una parte por ID' })
   @ApiParam({ name: 'creation_id', type: String })
@@ -118,5 +93,28 @@ export class PartsController {
       paginationDto,
       showDrafts: false,
     });
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Obtener una parte pública por ID' })
+  @ApiParam({ name: 'creation_id', type: String })
+  @ApiParam({ name: 'id', type: String })
+  @ApiResponse({ status: 200, type: GetPartResponseDto })
+  @ApiResponse({ status: 404, description: 'Not Found' })
+  findOne(@Param('creation_id') creation_id: string, @Param('id', ParseUUIDPipe) id: string) {
+    return this.partsService.findOne({ creation_id, id, showDrafts: false });
+  }
+
+  @Get('author/:id')
+  @ApiOperation({
+    summary: 'Obtener una parte (incluyendo borradores) del autor o colaborador por ID',
+  })
+  @ApiParam({ name: 'creation_id', type: String })
+  @ApiParam({ name: 'id', type: String })
+  @ApiResponse({ status: 200, type: GetPartResponseDto })
+  @ApiResponse({ status: 404, description: 'Not Found' })
+  @AuthenticateByAuthorOwnership(VALID_ROLES.ORIGINAL_AUTHOR, VALID_ROLES.COLLABORATOR)
+  findMyPart(@Param('creation_id') creation_id: string, @Param('id', ParseUUIDPipe) id: string) {
+    return this.partsService.findOne({ creation_id, id, showDrafts: true });
   }
 }
